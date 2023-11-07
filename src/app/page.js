c98c60f6702d1216651a898ad8fc1917cd7cf276
tomBlaine <tom.blaine2002@gmail.com>
2023-11-07 15:44:59 +0000

    added hero and basic styles

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,10 @@
 import Image from 'next/image'
+import HeroSection from './components/HeroSection'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen">
-      <h1></h1>
+    <main className="flex min-h-screen flex-col bg-[#121212] container mx-autox px-12 py-4">
+      <HeroSection></HeroSection>
     </main>
   )
 }
