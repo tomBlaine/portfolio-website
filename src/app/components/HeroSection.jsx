4b070b84ab9093b234619268c5102cbd9d9fa478
tomBlaine <tom.blaine2002@gmail.com>
2023-11-14 10:57:17 +0000

    added css to hero

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -1,19 +1,36 @@
 import React from 'react'
-
+import Image from 'next/image'
 const HeroSection = () => {
   return (
     <section>
         <div className="grid grid-cols-1 lg:grid-cols-12">
 
-            <div className="col-span-7 place-self-center">
-                <h1 className="text-white mb-4 text-4xl lg:text-6xl font-extrabold">
-                    Hi, Im Tom.
+            <div className="col-span-7 place-self-center text-centre sm:text-left">
+                <h1 className="text-white mb-4 text-4xl lg:text-6xl sm:text-5xl font-extrabold">
+                    <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">Hello, Im{" "}</span>
+                    Tom
                 </h1>
-                <p className="text-[#ADB7BE] text-lg lg:text-xl">
+                <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
                     sjdjadkljs sjnadjnsan sjandjsnajd jksandjksa jksndjdnsakj
                 </p>
+                <div>
+                    <button className="px-6 py-3 rounded-full w-full sm:w-fit mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white">Hire me</button>
+                    <button className="px-1 py-1 rounded-full w-full sm:w-fit bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500  hover:bg-slate-800 text-white  mt-3">
+                        <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">Download CV</span>
+                    </button>
+                </div>
             </div>
-            <div className="col-span-5"></div>
+            <div className="col-span-5 place-self-center mt-4 lg:mt-0">
+                <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
+                    <Image
+                      src="/images/hero-image1.png"
+                      alt="hero image"
+                      className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+                      width={300}
+                      height={300}
+                    />
+                </div>
+            </div>
 
         </div>
     </section>
--- src/app/page.js
+++ src/app/page.js
@@ -3,8 +3,10 @@ import HeroSection from './components/HeroSection'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col bg-[#121212] container mx-autox px-12 py-4">
-      <HeroSection></HeroSection>
+    <main className="flex min-h-screen flex-col bg-[#121212]">
+      <div class="container mx-autox px-12 py-4">
+        <HeroSection></HeroSection>
+      </div>
     </main>
   )
 }
