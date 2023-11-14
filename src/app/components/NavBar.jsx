a8fcac96fe360515e65d7f5b38d4b3d1afb88f3b
tomBlaine <tom.blaine2002@gmail.com>
2023-11-14 12:05:50 +0000

    added state to navbar button and icons

--- src/app/components/NavBar.jsx
+++ src/app/components/NavBar.jsx
@@ -2,6 +2,7 @@
 import Link from 'next/link'
 import React, { useState } from 'react'
 import NavLink from './NavLink'
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 
 const navLinks = [
     {
@@ -22,8 +23,21 @@ const NavBar = () => {
   const [navbarOpen, setNavbarOpen] = useState(false);
   return (
     <nav className="fixed top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-90">
-        <div className="flex flex-wrap items-center justify-between mx-auto px-4">
+        <div className="flex flex-wrap items-center justify-between mx-auto px-4 py-2">
             <Link href={"/"} className="text-2xl md:text-5xl text-white font-semibold">LOGO</Link>
+            <div className="mobile-menu block md:hidden">
+                {
+                    !navbarOpen ? (
+                        <button onClick={ () => setNavbarOpen(true) } className="flex items-center px-3 py-2 border rounded border-slate-200  text-slate-200 hover:text-white">
+                            <Bars3Icon className="h-5 w-5"></Bars3Icon>
+                        </button>
+                    ) : (
+                        <button onClick={ () => setNavbarOpen(false) } className="flex items-center px-3 py-2 border rounded border-slate-200  text-slate-200 hover:text-white">
+                            <XMarkIcon className="h-5 w-5" />
+                        </button>
+                    )
+                }
+            </div>
             <div className="menu hidden md:block md:w-auto" id="navbar">
                 <ul className="flex p-4 md:p-0 md:flex-row md:space-x-8 mt-0">
                     {
@@ -42,3 +56,4 @@ const NavBar = () => {
 }
 
 export default NavBar
+
