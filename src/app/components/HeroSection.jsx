191bce6f51bf2c10cf246f2fa684c837c4616ca0
tomBlaine <tom.blaine2002@gmail.com>
2023-11-22 15:04:22 +0000

    style changes and installed framer

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -2,14 +2,15 @@
 import React from 'react';
 import Image from 'next/image';
 import { TypeAnimation } from 'react-type-animation';
+import { motion } from "framer-motion";
 const HeroSection = () => {
   return (
-    <section>
+    <section className="lg:py-16 ">
         <div className="grid grid-cols-1 lg:grid-cols-12">
 
-            <div className="col-span-7 place-self-center text-centre sm:text-left">
-                <h1 className="text-white mb-4 text-4xl lg:text-6xl sm:text-5xl font-extrabold">
-                    <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">Hello, Im{" "}</span>
+            <div className="col-span-8 place-self-center text-centre sm:text-left justify-self-start">
+                <h1 className="text-white mb-4 text-4xl lg:text-7xl lg:leading-normal sm:text-5xl font-extrabold">
+                    <span className="text-transparent bg-clip-text bg-gradient-to-r from-primary-400 to-secondary-600">Hello, Im{" "}</span>
                     <br/>
                     <TypeAnimation
       sequence={[
@@ -37,7 +38,7 @@ const HeroSection = () => {
                     </button>
                 </div>
             </div>
-            <div className="col-span-5 place-self-center mt-4 lg:mt-0">
+            <div className="col-span-4 place-self-center mt-4 lg:mt-0">
                 <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
                     <Image
                       src="/images/hero-image1.png"
