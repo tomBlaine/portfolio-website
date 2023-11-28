83c8fe26770a4c1fc07ca40d2bbc4f111abb0558
tomBlaine <tom.blaine2002@gmail.com>
2023-11-28 18:00:55 +0000

    added CV and logo

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-
+import Image from 'next/image';
 const Footer = () => {
   return (
     <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
-        <div className="container p-12 flex justify-between">
-            <span>Thomas Blaine</span>
-            <p className="text-slate-600">Created by Thomas Blaine 2023</p>
+        <div className="container p-6 flex justify-between">
+            <Image src="/images/logo.png" width={50} height={50}/>
+            <p className="text-slate-600 mt-2">Created by Thomas Blaine 2023</p>
         </div>
     </footer>
   )
--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -38,9 +38,11 @@ const HeroSection = () => {
                 </p>
                 <div>
                     <button className="px-6 py-3 rounded-full w-full sm:w-fit mr-4 bg-gradient-to-br from-blue-500 via-primary-500 to-secondary-500 hover:bg-slate-200 text-white">Hire me</button>
-                    <button className="px-1 py-1 rounded-full w-full sm:w-fit bg-gradient-to-br from-blue-500 via-primary-500 to-secondary-500  hover:bg-slate-800 text-white  mt-3">
-                        <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">Download CV</span>
-                    </button>
+                    <a href="/cv.pdf" download>
+                      <button className="px-1 py-1 rounded-full w-full sm:w-fit bg-gradient-to-br from-blue-500 via-primary-500 to-secondary-500  hover:bg-slate-800 text-white  mt-3">
+                          <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">Download CV</span>
+                      </button>
+                    </a>
                 </div>
             </motion.div>
             <motion.div 
