fd6ec41e67b037b1c99f5a389f8293b4d9b6bdac
tomBlaine <tom.blaine2002@gmail.com>
2023-11-28 21:31:38 +0000

    removed type animation and added hero image blob

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -1,8 +1,15 @@
 "use client";
 import React from 'react';
 import Image from 'next/image';
-import { TypeAnimation } from 'react-type-animation';
 import { motion } from "framer-motion";
+
+const variants = {
+  animate: {
+    borderRadius: ["60% 40% 30% 70% / 60% 30% 70% 40%", "30% 60% 70% 40% / 50% 60% 30% 60%", "60% 40% 30% 70% / 60% 30% 70% 40%"],
+    transition: { duration: 8, repeat: Infinity, ease: "easeInOut" }
+  }
+};
+
 const HeroSection = () => {
   return (
     <section id="hero" className="lg:py-10 scroll-mt-24 lg:ml-20">
@@ -17,21 +24,7 @@ const HeroSection = () => {
                 <h1 className="text-white mb-4 text-4xl lg:text-7xl lg:leading-normal sm:text-5xl font-extrabold">
                     <span className="text-transparent bg-clip-text bg-gradient-to-r from-primary-400 to-secondary-600">Hello, Im{" "}</span>
                     <br/>
-                    <TypeAnimation
-      sequence={[
-        'Tom',
-        1000,
-        'Mobile Developer',
-        1000,
-        'Web Developer',
-        1000,
-        'UI/UX Designer',
-        1000
-      ]}
-      wrapper="span"
-      speed={50}
-      repeat={Infinity}
-    />
+                    Tom
                 </h1>
                 <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
                     sjdjadkljs sjnadjnsan sjandjsnajd jksandjksa jksndjdnsakj
@@ -51,14 +44,12 @@ const HeroSection = () => {
               transition= {{ duration: 0.6 }}
               className="col-span-4 place-self-center mt-4 lg:mt-0"
             >
-                <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
-                    <Image
-                      src="/images/hero-image1.png"
-                      alt="hero image"
-                      className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-                      width={300}
-                      height={300}
-                    />
+                <div className="relative">
+                <motion.div
+                    className="bg-[url('/images/mac.jpeg')] w-[25rem] h-[25rem] bg-cover bg-cente border-primary-500 border-4 shadow-md absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+                    variants={variants}
+                    animate="animate"
+                />
                 </div>
             </motion.div>
 
